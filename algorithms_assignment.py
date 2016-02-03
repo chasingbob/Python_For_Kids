64e2139c091d599982690ca99ab3be96923d9934
Dries <dries.cronje@outlook.com>
2016-02-03 20:50:17 +0200

    Algorithms - Selection

---
+++ algorithms_assignment.py
@@ -0,0 +1,4 @@
+age = 10
+name = "John"
+
+print("%s is %d years old" % (name, age))
---
+++ algorithms_selection.py
@@ -0,0 +1,11 @@
+age = 17
+name = "John"
+
+if (age < 20):
+    print ("%s is in school" % name)
+else
+    print ("%s is too old for school" % name)
+    
+    
+    
+# Excercise: Change the value of the age variable to 21 and see how it affects the if-statement
