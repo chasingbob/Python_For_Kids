15e759d9014a11cfe543ca2bc722379ff0106b0c
dcronje <dcronje@email.com>
2017-01-24 17:57:31 +0200

    added calculator

---
+++ calculator.py
@@ -0,0 +1,28 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a/b
+
+
+# Class examples
+res1 = add(10, 20)
+print(res1)
+
+res2 = sub(373484, 1500)
+print(res2)
+
+res3 = mul(10, 3)
+print(res3)
+
+res4 = mul(mul(mul(mul(10, 10),10),10),10)
+print(res4)
+
+res5 = div(22.0, 7.0)
+print(res5)
