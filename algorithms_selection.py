47f0a33c50f1d0d74904e16faa6b4ce0f1181443
dcronje <dcronje@email.com>
2017-04-19 13:47:43 +0200

    updated *_repitition.py and *_selection.py to Python 3, and added examples

--- algorithms_selection.py
+++ algorithms_selection.py
@@ -1,11 +1,20 @@
+
+
+def print_appropriate_message(age, name):
+    if (age < 10):
+        print('{} is in primary school'.format(name))
+    elif (age >= 10 and age < 20):
+        print('{} is most likely in high school'.format(name))
+    else:
+        print('{} is too old for school'.format(name))
+
+
 age = 17
 name = "John"
 
-if (age < 20):
-    print ("%s is in school" % name)
-else
-    print ("%s is too old for school" % name)
+
+print_appropriate_message(age, name)
+print_appropriate_message(8, 'Angie')
+print_appropriate_message(88, 'Grandpa Jack')
+
     
-    
-    
-# Excercise: Change the value of the age variable to 21 and see how it affects the if-statement
